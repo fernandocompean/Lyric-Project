448f1992c29b7e2812ad9e7cb4350eb3fe0a8b94
Macbook Air de Luis Fernando <Fer@Fernandos-MacBook-Air.local>
2019-10-31 12:50:18 -0600

    [chore]: lyric component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { LyricsComponent } from './components/lyrics/lyrics.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LyricsComponent
   ],
   imports: [
     BrowserModule,
