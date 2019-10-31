416f94d8380c07af67c4f5d4393b8d12728045fa
Macbook Air de Luis Fernando <Fer@Fernandos-MacBook-Air.local>
2019-10-31 13:00:07 -0600

    [chore]: routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LyricsComponent } from './components/lyrics/lyrics.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "lyrics", component: LyricsComponent },
+  { path: "", pathMatch: "full", redirectTo: "home" },
+  { path: "**", pathMatch: "full", redirectTo: "home" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
