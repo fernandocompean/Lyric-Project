9822f5ceb9a1ccfe233b74c61eff7c6534972ada
Macbook Air de Luis Fernando <Fer@Fernandos-MacBook-Air.local>
2019-11-02 17:44:38 -0600

    [feature]: showLyric

--- src/app/components/lyrics/lyrics.component.ts
+++ src/app/components/lyrics/lyrics.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { LyricsService } from "../../services/lyrics.service";
+
 
 @Component({
-  selector: 'app-lyrics',
-  templateUrl: './lyrics.component.html',
-  styleUrls: ['./lyrics.component.css']
+  selector: "app-lyrics",
+  templateUrl: "./lyrics.component.html",
+  styleUrls: ["./lyrics.component.css"]
 })
 export class LyricsComponent implements OnInit {
 
-  constructor() { }
+  lyrics: string;
+
+  constructor(private lyricsApi: LyricsService) {}
 
   ngOnInit() {
+    this.showLyric();
+  }
+
+  showLyric() {
+    this.lyrics = this.lyricsApi.getLyric();
   }
 
 }
