8972ba99e2eeae1f67aa4855d571f1f15fad16ee
Macbook Air de Luis Fernando <Fer@Fernandos-MacBook-Air.local>
2019-11-02 17:43:58 -0600

    [feature]: getLyric navigate to lyric page n error

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { LyricsService } from '../../services/lyrics.service';
+import { NzModalService } from "ng-zorro-antd/modal";
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  artist: string;
+  song: string;
 
-  constructor() { }
+  constructor(
+    private lyricsApi: LyricsService,
+    private router: Router,
+    private modalService: NzModalService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  getLyric() {
+    this.lyricsApi
+      .GetLyric(`https://api.lyrics.ovh/v1/${this.artist}/${this.song}`)
+      .subscribe(
+        (data: any) => {
+          this.lyricsApi.setLyric(data.lyrics);
+          this.router.navigateByUrl('/lyrics');
+        },
+        error => {
+          this.error();
+        }
+      );
   }
 
+  error(): void {
+    this.modalService.error({
+      nzTitle: "Error",
+      nzContent: "Lo sentimos ;(, no pudimos encontrar la letra de la canción"
+    });
+  }
+
+
 }
+
+
+
